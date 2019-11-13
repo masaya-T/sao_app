1160aa639b366e245ec72b8de0d3e90f2dc39eb6
masaya-T <tapgjtwmdkpl@gmail.com>
2019-11-13 16:34:13 +0900

    l

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
-                    text: '${year}-${month}-${day} ${hour}:${minute}:${second}'
+                    text: year
                 }));
             }
         }
